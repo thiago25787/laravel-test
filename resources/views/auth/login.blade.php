05316f2bcf93f8b36e46c6156dbb515486258e62
Thiago Almeida Santos <thiago25787@gmail.com>
2021-08-02 17:34:34 -0300

    remove the link "Forgot your password?"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,11 +39,11 @@
             </div>
         </form>
         <div class="flex items-center justify-between mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+{{--            @if (Route::has('password.request'))--}}
+{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                    {{ __('Forgot your password?') }}--}}
+{{--                </a>--}}
+{{--            @endif--}}
             <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
                 {{ __('Register') }}
             </a>
